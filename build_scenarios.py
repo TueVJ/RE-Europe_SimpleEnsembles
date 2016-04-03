9472d430a76fd3d29adbac652076ba6ecd97400b
TueVJ <tue.vissing.jensen@gmail.com>
2016-04-03 22:50:16 +0200

    add sketch of scenario builder

---
+++ build_scenarios.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+""" Extracts RES-Europe data to examine forecasting issues.
+"""
+
+import numpy as np
+import pandas as pd
+import networkx as nx
+import os
+
+from load_network import load_network
+from scipy.stats import norm
+
+__copyright__ = "Copyright 2016, Tue Vissing Jensen"
+__credits__ = ["Tue Vissing Jensen"]
+__license__ = "MIT"
+__version__ = "1.0"
+__maintainer__ = "Tue Vissing Jensen"
+__email__ = "tvjens@elektro.dtu.dk"
+__status__ = "Prototype"
+
+# category = 'wind'
+category = 'solar'
+
+store = pd.HDFStore('covariance.h5')
+cov = store['/'.join((category, 'empirical'))]
+store.close()
+
+
+
+
+vs = np.random.multivariate_normal([0]*len(cov), cov.values, 100)
+unfvs = norm.ppf(vs)
