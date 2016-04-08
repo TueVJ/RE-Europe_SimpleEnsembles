2318f56178a3b20c53fd56ca692b29a3dc775a0e
TueVJ <tue.vissing.jensen@gmail.com>
2016-04-08 18:04:30 +0200

    initial structure

--- build_scenarios.py
+++ build_scenarios.py
@@ -8,7 +8,7 @@ import networkx as nx
 import os
 
 from load_network import load_network
-from scipy.stats import norm
+from scipy.stats import norm, beta
 
 __copyright__ = "Copyright 2016, Tue Vissing Jensen"
 __credits__ = ["Tue Vissing Jensen"]
@@ -21,11 +21,23 @@ __status__ = "Prototype"
 category = 'wind'
 # category = 'solar'
 
+NUM_SCENARIOS = 100
+scenarios = ['n' + str(i) for i in np.arange(NUM_SCENARIOS)]
 store = pd.HDFStore('covariance.h5')
 cov = store['/'.join((category, 'empirical'))]
 store.close()
 
-# Generate 100 samples with marginal normal distribution and the measured covariance.
-vs = pd.DataFrame(np.random.multivariate_normal([0]*len(cov), cov.values, 100), columns=cov.columns)
-# Convert these samples to uniformly distributed values
-unfvs = norm.cdf(vs)
+# Point forecasts [n,k] -> pfc
+# pfcs = # LOAD FROM STORE
+
+for k in ks:
+    # Generate NUM_SCENARIOS samples with marginal normal distribution and the measured covariance.
+    vs = pd.DataFrame(np.random.multivariate_normal([0]*len(cov), cov.values, NUM_SCENARIOS), columns=cov.columns, index=scenarios)
+    # Convert these samples to uniformly distributed values
+    unfvs = norm.cdf(vs)
+    for n, col in unfvs.iteritems():
+        outcol = beta.ppf(
+            col,
+            alphapanel[n, k].interp(pfcs[n, k]),
+            betapanel[n, k].interp(pfcs[n, k])
+        )
